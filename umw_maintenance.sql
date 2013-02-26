05f3cec18ad7a9c9d3a8f11dd907cda8df7a1984
Singularity <singularity@portal-of-singularity.(none)>
2013-02-25 23:02:04 -0500

    Login php working and updated source file login with user admin and password admin

--- umw_maintenance.sql
+++ umw_maintenance.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 21, 2013 at 02:10 PM
+-- Generation Time: Feb 25, 2013 at 10:59 PM
 -- Server version: 5.1.63
 -- PHP Version: 5.3.5-1ubuntu7.11
 
@@ -18,7 +18,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Database: `umw_maintenance`
 --
-CREATE DATABASE `umw_maintenance`;
+CREATE DATABASE `umw_maintenance` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `umw_maintenance`;
 
 -- --------------------------------------------------------
@@ -37,9 +37,30 @@ CREATE TABLE IF NOT EXISTS `reports` (
   `Floor` varchar(40) DEFAULT NULL,
   `Room` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `reports`
 --
 
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(80) NOT NULL,
+  `permissions` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `permissions`) VALUES
+(1, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 0);
