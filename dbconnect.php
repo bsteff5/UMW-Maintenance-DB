4a1a656314443c7936a46d547188ac0338072a48
Singularity <singularity@portal-of-singularity.(none)>
2013-02-26 12:42:37 -0500

    added submitReport.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php
 
-	$db = mysqli_connect('localhost', 'root', '$hkeyX.7125.X', 'umw_maintenance') or die("Failed to connect to the Database");
+	$db = mysqli_connect('localhost', 'root', '', 'umw_maintenance') or die("Failed to connect to the Database");
 
 ?>
