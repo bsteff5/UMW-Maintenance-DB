ae26c2520006e42cc41899c36bf68a545a7de74f
Patrick Mullen <li_pat_li@yahoo.com>
2013-03-19 13:19:42 -0400

    fixed submitting
    asasdasda

--- submitReport.php
+++ submitReport.php
@@ -9,8 +9,9 @@ $ReportDate=$_POST['year']."-".$_POST['month']."-".$_POST['day'];
 $Description=$_POST['Description'];
 				
  				
-				$q = mysqli_query($db, "INSERT INTO reports (Description,ReportDate,Building,Floor,Room) VALUES ('$Description','$ReportDate','$Building','$Floor','$Room')") or die("Couldn't query the Database or query failed"); 
-				
+				$q = mysqli_query($db, "INSERT INTO reports (Description,ReportDate) VALUES ('$Description','$ReportDate')") or die("Couldn't query the Database or query failed"); 
+				$q = mysqli_query($db, "INSERT INTO rooms (room) VALUES ('$Room')") or die("Couldn't query the Database or query failed");
+				$q= mysqli_query($db, "INSERT INTO room_problems (room) VALUES ('$Room')") or die("Couldn't query the Database or query failed");
 				if($q == TRUE) { 
 					echo "successful"; 
 				}else { 
